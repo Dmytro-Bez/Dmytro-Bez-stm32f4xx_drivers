a85f9bfb7b3be01d64e88bd126edfa8d2b06c183
Dmytriy <61518737+Dmytro-Bez@users.noreply.github.com>
2026-02-24 17:33:42 +0200

    problems

--- stm32f4xx_drivers/Debug/Src/main.d
+++ stm32f4xx_drivers/Debug/Src/main.d
@@ -1,3 +1,7 @@
 Src/main.o: ../Src/main.c \
- /home/chunin1988/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
-/home/chunin1988/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+ C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
+ C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
+C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
--- stm32f4xx_drivers/Debug/drivers/Src/stm32f4xx_gpio_driver.d
+++ stm32f4xx_drivers/Debug/drivers/Src/stm32f4xx_gpio_driver.d
@@ -1,6 +1,8 @@
 drivers/Src/stm32f4xx_gpio_driver.o: \
  ../drivers/Src/stm32f4xx_gpio_driver.c \
- /home/chunin1988/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
- /home/chunin1988/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
-/home/chunin1988/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
-/home/chunin1988/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+ C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
+ C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h
+C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
+C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+C:/Users/dzimi/Documents/GitHub/Dmytro-Bez-stm32f4xx_drivers/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
